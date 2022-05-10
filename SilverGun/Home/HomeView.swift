98fd09e2160f99d464cf1a13f7c7fd6c6e0f29a7
gayoenLee <leeedho67@gmail.com>
2022-05-10 21:44:43 +0900

    [MOD]홈뷰 레이아웃 오류 수정 및 snapkit추가

--- SilverGun/Home/HomeView.swift
+++ SilverGun/Home/HomeView.swift
@@ -7,17 +7,27 @@
 
 import Foundation
 import UIKit
+import SnapKit
 
-
-class HomeView : ViewController {
+final class HomeView : UIView {
     
     var carBtn = UIButton()
     
-    override func viewDidLoad() {
-        view.translatesAutoresizingMaskIntoConstraints = false
-        self.view?.addSubview(carBtn)
-        carBtn.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        carBtn.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupLayout() {
+        
+        self.addSubview(carBtn)
+        carBtn.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        carBtn.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
         carBtn.widthAnchor.constraint(equalToConstant: 300).isActive = true
         carBtn.heightAnchor.constraint(equalToConstant: 100).isActive = true
     }
