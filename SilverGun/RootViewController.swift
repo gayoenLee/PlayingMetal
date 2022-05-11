d4c7765e5a2e5686ca486c99271da3ff3018c2b2
gayoenLee <leeedho67@gmail.com>
2022-05-11 21:38:49 +0900

    [ADD]루트뷰컨트롤러로 추후에 탭바로 다른 컨트롤러도 추가 예정

---
+++ SilverGun/RootViewController.swift
@@ -0,0 +1,46 @@
+//
+//  RootViewController.swift
+//  SilverGun
+//
+//  Created by 이은호 on 2022/05/11.
+//
+
+import Foundation
+import UIKit
+import Then
+
+final class RootViewController : UITabBarController {
+    
+    override func viewDidLoad() {
+        print("rootviewController viewDidLoad")
+        super.viewDidLoad()
+        setupTabBar()
+        setupTabBarViewControllers()
+    }
+    
+    private func setupTabBar(){
+        self.tabBar.do{
+            $0.barTintColor = .white
+            $0.isTranslucent = false
+            $0.tintColor = .primary1
+            $0.unselectedItemTintColor = .monogrey1
+        }
+    }
+    
+    
+    private func setupTabBarViewControllers(){
+        let viewControllers = [homeViewController]
+        self.setViewControllers(viewControllers, animated: false)
+    }
+    
+    private lazy var homeViewController: UINavigationController = {
+        let homeViewModel = HomeViewModel()
+        let homeVC = HomeViewController(viewModel: homeViewModel).then{
+            $0.tabBarItem = UITabBarItem(title: "", image: UIImage(named: ""), selectedImage: nil)
+        }
+        let navigationController = UINavigationController(rootViewController: homeVC)
+        navigationController.setNavigationBarHidden(true, animated: false)
+        navigationController.modalPresentationStyle = .fullScreen
+        return navigationController
+    }()
+}
