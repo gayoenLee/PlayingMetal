5354bd910bc2de3974a914cd150dc82e19ef5652
gayoenLee <leeedho67@gmail.com>
2022-05-11 21:37:15 +0900

    [MOD]홈뷰컨트롤러에 뷰 세팅

--- SilverGun/Home/HomeViewController.swift
+++ SilverGun/Home/HomeViewController.swift
@@ -11,18 +11,30 @@ import UIKit
 class HomeViewController: UIViewController {
     
     private let viewModel: HomeViewModel
-    private let homeView = HomeView()
+    private let homeView = HomeView(frame: .zero)
     
     init(viewModel: HomeViewModel) {
         self.viewModel = viewModel
         
         super.init(nibName: nil, bundle: nil)
         self.viewModel.delegate = self
+        self.setupHomeView()
     }
     
     required init?(coder: NSCoder){
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private func setupHomeView(){
+        self.view.addSubview(homeView)
+        self.homeView.snp.makeConstraints{
+            $0.edges.equalToSuperview()
+        }
+        self.homeView.then{
+            $0.backgroundColor = .tintColor
+        }
+    }
+    
 }
 
 extension HomeViewController : HomeViewModelDelegate {
