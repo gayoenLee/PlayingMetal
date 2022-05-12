21cb1ddb26f4b8ef107b78a02f20f69b56fb17ec
gayoenLee <leeedho67@gmail.com>
2022-05-12 22:16:54 +0900

    [ADD]자동차 메탈 뷰 추가

---
+++ SilverGun/CarMetal/CarView.swift
@@ -0,0 +1,22 @@
+//
+//  CarView.swift
+//  SilverGun
+//
+//  Created by 이은호 on 2022/05/12.
+//
+
+import UIKit
+import SnapKit
+import MetalKit
+
+final class CarView : MTKView {
+    
+    var renderer : RendererHelper!
+    
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
