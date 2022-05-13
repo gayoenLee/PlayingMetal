749e62e4ad062e5200d37413893c6ebde1edb5b6
gayoenLee <leeedho67@gmail.com>
2022-05-13 23:07:22 +0900

    [MOD]메탈 렌더링 생성 메소드 추가

--- SilverGun/CarMetal/CarView.swift
+++ SilverGun/CarMetal/CarView.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SnapKit
 import MetalKit
-
+//상속이 불가하도록 final로 설정
 final class CarView : MTKView {
     
     var renderer : RendererHelper!
@@ -16,6 +16,10 @@ final class CarView : MTKView {
     
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func createRenderer(device : MTLDevice!, metalView : MTKView){
+        renderer = RendererHelper()
     }
     
 }
