347b084d9ba534009363d375ee737bdc84f51596
gayoenLee <leeedho67@gmail.com>
2022-05-16 21:46:32 +0900

    [MOD]뷰컨트롤러 - MTKViewDelegate로 설정하면 device, render pipelein을 세팅하는 것이 어려워서 변경함.

--- SilverGun/CarMetal/CarViewController.swift
+++ SilverGun/CarMetal/CarViewController.swift
@@ -12,18 +12,15 @@ import MetalKit
 
 class CarViewController: UIViewController {
     
-    var mtkView: CarView!
-    var commandQueue : MTLCommandQueue!
     private let viewModel : CarViewModel
-    
     
     init(viewModel: CarViewModel){
         self.viewModel = viewModel
         
         super.init(nibName: nil, bundle: nil)
-        self.viewModel.delegate = self
+        //self.viewModel.delegate = self
         //mtkView = CarView(frame: self.view.bounds)
-        mtkView.device = MTLCreateSystemDefaultDevice()
+        
         
     }
     
@@ -31,28 +28,4 @@ class CarViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func setupMetalView(){
-    
-    }
-}
-
-extension CarViewController: CarViewModelDelegate {
-    func draw(in view: MTKView) {
-        render()
-    }
-    
-    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
-        print("")
-    }
-    
-    func render(){
-        guard let renderPassDescriptor = mtkView.currentRenderPassDescriptor else { return }
-        guard let commandBuffer = commandQueue.makeCommandBuffer() else { return }
-        guard let commandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor) else { return }
-        guard let currentDrawble = mtkView.currentDrawable else { return }
-        
-        commandEncoder.endEncoding()
-        commandBuffer.present(currentDrawble)
-        commandBuffer.commit()
-    }
 }
