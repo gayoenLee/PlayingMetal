505585f5010fd335a4b58a807e18f308abe492fb
gayoenLee <leeedho67@gmail.com>
2022-05-16 21:45:35 +0900

    [ MOD]MTKViewDelegate를 따르는 CarViewModel을 delegate로 설정

--- SilverGun/CarMetal/CarView.swift
+++ SilverGun/CarMetal/CarView.swift
@@ -11,15 +11,27 @@ import MetalKit
 //상속이 불가하도록 final로 설정
 final class CarView : MTKView {
     
-    var renderer : RendererHelper!
+    var viewModel : CarViewModel!
     
+     init(){
+         //MTKView에서는 super.init()콜시에 디바이스를 꼭 세팅해줘야함.
+         super.init(frame: .zero, device: MTLCreateSystemDefaultDevice())
+         
+        guard let device = MTLCreateSystemDefaultDevice() else {
+            fatalError("Metal device loading error")
+            return
+        }
+       createRenderer(device: device, metalView: self)
+         
+    }
     
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func createRenderer(device : MTLDevice!, metalView : MTKView){
-        renderer = RendererHelper()
+    func createRenderer(device : MTLDevice!, metalView : CarView){
+        viewModel = CarViewModel(view: metalView, device: device)
+        delegate = viewModel
     }
     
 }
